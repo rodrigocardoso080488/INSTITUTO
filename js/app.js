de0197e59756750b0da6c9e05ae2a5c945a8d31f
rodrigocardoso <rodrigocardoso080488@gmail.com>
2023-11-26 20:58:49 -0300

    primeros conceptos de js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-alert("hola rolling")
+console.log("hola rodrigo");
+
+let mensaje="rodrigo"
+
+mensaje="zole";
+
+console.log("mi variable guarda el valor",mensaje);
+
+document.write("hola rodrigo")
+document.write('<br>'+mensaje)
